70c428373e055f9b0e60373fd70d74bd46b27d21
mahbuba <mahbubaislam7010@gmail.com>
2022-04-18 05:46:22 -0400

    blogs-done

--- src/Pages/Blogs/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs/Blogs.js
@@ -3,19 +3,24 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h2>My Goals In My Life</h2>
-            <div className="container">
-               
-                <p>A person needs to achieve certain goals in one's life before you can call them successful. 
-                 is to achieve goals, you have set. I have set certain goals i would like to achieve my lifetime.
-                , The most important goal i want to achieve in my life is to be a professional programmer. 
-                But, this journey is not easy for me. Although i have found a great course to be a programmer.
-                But every day i face so much problem to finish classes. But i didn't give up and i promise to me that in future also
-                i won't give up. I always believe in my-self that if i work hard Oneday i will be a great programmer InSha-Allah
-                I will always work-hard to fulfill my dream no matter what happen in my life.</p> 
-             
-            </div>
+            <h2>Authentication vs. authorization</h2>
+           <div>
+          <h3>Authentication</h3>
+          <p>Authentication is the process of determining the users identity via the available credentials, thus verifying the identity. Some ways to authenticate one's identity are listed here:
+          Usernames or passwords can be used to establish one's identity, thus gaining access to the system.
+        OTPs are another way to get access to the system for a single transaction
+        Apps that generate security codes via the third party, thus enabling access for the user
+         Biometrics such as an eye scan or fingerprints can be used to gain access</p>
+          <div>
+              <h3>authorization</h3>
+              <p>The act of validating the users are the same which they are claiming to be is called authentication. This is the very initial stage of any security process. A basic example of authentications is providing someone with permission to download a file from the server after providing the credentials or providing access to individual users to an application. On the other hand, in the process of system security, the process of providing access to a specific resource or function is called authorization.</p>
+          </div>
+           </div> 
+           <h3>why are you using firebase?</h3>
+           <p>Firebase is a famous product of Google which is used by so many developers to add backend functionality for their website as well as apps. It will make your job really easier for backend database and handling the database. It's by Google can be used for the following: Firebase manages all data real-time in the database.Firebase can be used when you do not want to spend a lot of time developing your own backend. It can be used for rapid development. I have been using it in my apps in production. You will not have to take care of servers, etc, 
+</p>
         </div>
+        
     );
 };
 
--- src/Pages/Home/Home/Banner/Banner.js
+++ src/Pages/Home/Home/Banner/Banner.js
@@ -27,26 +27,26 @@ const Banner = () => {
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
+          
             <Carousel.Item>
                 <img
                     className="d-block w-100"
                     src={picture2}
                     alt="Second slide"
                 />
-
-                <Carousel.Caption>
+            <Carousel.Caption>
                     <h3>Second slide label</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
+            </Carousel.Caption>
             </Carousel.Item>
+           
             <Carousel.Item>
                 <img
                     className="d-block w-100 "
                     src={picture3}
                     alt="Third slide"
                 />
-
-                <Carousel.Caption>
+            <Carousel.Caption>
                     <h3>Third slide label</h3>
                     <p>
                         Praesent commodo cursus magna, vel scelerisque nisl consectetur.
